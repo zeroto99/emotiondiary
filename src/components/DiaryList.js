9f0225b19fae21a54573def5a660beee319ac808
zeroto99 <myv_01@naver.com>
2022-04-13 01:56:50 +0900

    :memo: 홈 - 정렬(최신순/오래된순) 구현 완료

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,9 +1,51 @@
+import { useState } from "react";
+
+const sortOptionList = [
+  {value: 'latest', name: '최신순'},
+  {value: 'oldest', name: '오래된 순'},
+]
+
+const ControlMenu = ({value, onChange, optionList}) => {
+  return ( 
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((it, idx) => (
+        <option key={idx} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select> 
+  )
+}
+
 const DiaryList = ({diaryList}) => {
-  return <div>
-    {diaryList.map((it) => (
-      <div key={it.id}>{it.content}</div>
-    ))}
-  </div>
+
+  const [sortType, setSortType] = useState('lastest');
+
+  const getProcessedDiaryList = () => {
+    const compare = (a,b) => {
+      if(sortType === 'latest') {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    }
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+    const sortedList = copyList.sort(compare);
+    return sortedList;
+  }
+
+  return (
+    <div>
+      <ControlMenu 
+        value={sortType} 
+        onChange={setSortType} 
+        optionList={sortOptionList}
+      />
+      {getProcessedDiaryList().map((it) => (
+        <div key={it.id}>{it.content}</div>
+      ))}
+    </div>
+  )
 }
 
 DiaryList.defaultProps = {
