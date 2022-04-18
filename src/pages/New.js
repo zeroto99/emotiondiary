4293f4d80f888607caa6aa71f418cc3862287b55
zeroto99 <myv_01@naver.com>
2022-04-18 21:08:25 +0900

    :memo: 일기 쓰기(/new) 페이지 구현 완료

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,11 @@
+import DiaryEditor from "../components/DiaryEditor";
+
 const New = () => {
-  return <div>
-    <h1>New</h1>
-    <p>이곳은 일기 추가 작성 페이지입니다.</p>
-  </div>
+  return (
+    <div>
+      <DiaryEditor />
+    </div>
+  )
 }
 
 export default New;
