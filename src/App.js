f230c44a5ee32f6438d1bb69b323867f4c72db0f
zeroto99 <myv_01@naver.com>
2022-04-19 12:19:21 +0900

    :bug: 일기 아이템 key 값 중복 해결

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ function App() {
 
   const [data, dispatch] = useReducer(reducer, dummyData);
 
-  const dataId = useRef(0);
+  const dataId = useRef(6);
   
   // CREATE
   const onCreate = (date, content, emotion) => {
